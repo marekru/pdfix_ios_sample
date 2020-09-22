78b93ac99790bd9e566be02a0dbd2607a556b476
marekru <marekru@users.noreply.github.com>
2020-09-22 13:21:01 +0200

    Initial Commit

---
+++ RApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RApp
+//
+//  Created by Marek K on 22/09/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RApp
+//
+//  Created by Marek K on 22/09/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
